319f7af305cf0c3d3e1dcc0fa3696e653819bbd2
atul-was-here <atultiwariwashere@gmail.com>
2025-05-14 23:06:16 +0530

    Created CS50 Python Solutions repository

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+resource "github_repository" "github-infra" {
+ name        = "github-infra"
+ description = "My repository for managing GitHub via Terraform"
+ visibility  = "public"
+ auto_init   = true
+}
+
 resource "github_repository" "shell-scripts" {
  name        = "shell-scripts"
  description = "My repository for shell scripts"
@@ -5,9 +12,9 @@ resource "github_repository" "shell-scripts" {
  auto_init   = true
 }
 
-resource "github_repository" "github-infra" {
- name        = "github-infra"
- description = "My repository for managing GitHub via Terraform"
+resource "github_repository" "cs50-python" {
+ name        = "cs50-python"
+ description = "My notes and solutions for CS50 Python" 
  visibility  = "public"
  auto_init   = true
 }
